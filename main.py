4a5a968c26b90320c53fc6a34fe49e09b1062ac8
nikki-cmd <76265039+nikki-cmd@users.noreply.github.com>
2025-01-18 18:03:49 +0300

    Fix main

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from sklearn.model_selection import train_test_split
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+from sklearn.decomposition import PCA
+
 
 path = "dataset/bikes_rent.csv"
 
@@ -13,15 +15,22 @@ df = pd.read_csv(path)
 dataframe = pd.DataFrame(df)
 print(dataframe.columns)
 dataframe = dataframe.drop(labels=['season', 'atemp', 'windspeed(mph)'], axis=1)
-train, test = train_test_split(dataframe, test_size=0.2)
-X = train.drop(['cnt'], axis=1)
-y = train.drop(['yr', 'mnth', 'holiday', 'weekday', 'workingday',
-       'weathersit', 'temp', 'hum', 'windspeed(ms)'], axis=1)
+
+pca = PCA(n_components=1)
+df_pca = pca.fit_transform(dataframe.drop(['cnt'], axis=1))
+
+plt.scatter(df_pca, df['cnt'])
+plt.show()
 
 correlation_matrix = dataframe.corr()
 plt.figure(figsize=(10, 8))
 sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f")
 plt.show()
+
+train, test = train_test_split(dataframe, test_size=0.2)
+X = train.drop(['cnt'], axis=1)
+y = train.drop(['yr', 'mnth', 'holiday', 'weekday', 'workingday',
+       'weathersit', 'temp', 'hum', 'windspeed(ms)'], axis=1)
 
 
 
